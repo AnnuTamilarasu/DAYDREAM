b7a532414c1f32d1471fa76f1f58e936a95320e6
AnnuTamilarasu <ananya.tamilarasu@gmail.com>
2025-09-27 19:59:48 +0000

    Update HomePg.java

--- HomePg.java
+++ HomePg.java
@@ -1,14 +1,18 @@
 import javax.swing.*;
 
 public class HomePg extends JPanel {
+    private JLabel background;
+    private JButton play;
+    private JButton tutorial;
+
     public HomePg() {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));                                 
         
-        JButton play= new JButton("Click Me");
-        button.addActionListener(e -> System.out.println("Button Clicked!"));
+        play = new JButton("Click Me");
+        play.addActionListener(e -> System.out.println("Button Clicked!"));
         this.add(play);
 
-        JButton tutorial= new JButton("Another Button");
+        tutorial= new JButton("Another Button");
         tutorial.addActionListener(e -> System.out.println("Another Button Clicked!"));
         this.add(tutorial);
         
@@ -16,5 +20,11 @@ public class HomePg extends JPanel {
         this.add(label);
         this.setSize(400, 300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public void bg() {
+        ImageIcon img = new ImageIcon("path/to/your/image.jpg");
+        background = new JLabel("", img, JLabel.CENTER);
+        background.setBounds(0, 0, 400, 300);
+        this.add(background);
     }
 }
