abe93a0083449ff0cd935be88ee20b5d54045215
AnnuTamilarasu <ananya.tamilarasu@gmail.com>
2025-09-27 23:00:56 +0000

    Push all changes

--- HomePg.java
+++ HomePg.java
@@ -1,35 +1,65 @@
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class HomePg extends JPanel implements ActionListener {
+public class HomePg extends JFrame implements ActionListener {
 
     JButton play;
     JButton tutorial;
+    JPanel homePanel;
+    JLabel background;
+    String bgImgPath = "Untitled_design-removebg-preview.png";
 
     public HomePg() {
-        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));                                 
-        
-        play = new JButton("Click Me");
-        play.addActionListener(this);   // use actionPerformed
-        this.add(play);
+        this.setSize(1200, 765);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        tutorial= new JButton("Another Button");
-        tutorial.addActionListener(this);
-        this.add(tutorial);
-        
-        JLabel label = new JLabel("Welcome to the Home Page");
-        this.add(label);
-        this.setSize(400, 300);
+        homePanel = new JPanel(null); // null layout for manual positioning
+        this.add(homePanel);
+
+        bg();           // add background first
+        elements();     // then add buttons/label
+
+        this.setVisible(true);
     }   
+    
+    public void elements() {
+        play = new JButton("Play");
+        setupButton(play, 100, 200);
+        homePanel.add(play);
+
+        tutorial = new JButton("Tutorial");
+        setupButton(tutorial, 100, 400);
+        homePanel.add(tutorial);
+
+        JLabel label = new JLabel("Welcome to the Home Page");
+        label.setBounds(100, 100, 300, 40);
+        homePanel.add(label);
+    }
+
+    public void setupButton(JButton button, int xPos, int yPos) {
+        button.setBounds(xPos, yPos, 200, 50);
+        button.setBackground(new Color(222, 172, 159));
+        button.setBorderPainted(false);
+        button.setFocusPainted(false);
+        button.addActionListener(this);
+    }
+
+    public void bg() {
+        ImageIcon bgImg = new ImageIcon(bgImgPath);
+        background = new JLabel("", bgImg, JLabel.CENTER);
+        background.setBounds(0, 0, 1200, 765);
+        homePanel.add(background);
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == play) {
-            JFrame topFrame = (JFrame) SwingUtilities.getWindowAncestor(this);
-            topFrame.setContentPane(new GamePg()); 
-            topFrame.revalidate();
+            this.setContentPane(new GamePg());
+            this.revalidate();
+            this.repaint();
         } else if (e.getSource() == tutorial) {
-            new Tutorial(); // opens new window
+            new Tutorial();
         }
     }
 }
--- Main.java
+++ Main.java
@@ -2,13 +2,7 @@ import javax.swing.*;
 
 public class Main extends JFrame {
     public static void main(String[] args) {
-        JFrame mainFrame = new JFrame();
-        mainFrame.setSize(400, 300);
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        HomePg homePg = new HomePg();
-        mainFrame.add(homePg);
-
-        mainFrame.setVisible(true);
+        new HomePg();
+        new Audio();
     }
 }
