8e76a2d6e5f3a155707cf6c616a01d6a29849e2f
AnnuTamilarasu <ananya.tamilarasu@gmail.com>
2025-09-27 19:45:26 +0000

    Update HomePg.java

--- HomePg.java
+++ HomePg.java
@@ -3,6 +3,15 @@ import javax.swing.*;
 public class HomePg extends JPanel {
     public HomePg() {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));                                 
+        
+        JButton play= new JButton("Click Me");
+        button.addActionListener(e -> System.out.println("Button Clicked!"));
+        this.add(play);
+
+        JButton tutorial= new JButton("Another Button");
+        tutorial.addActionListener(e -> System.out.println("Another Button Clicked!"));
+        this.add(tutorial);
+        
         JLabel label = new JLabel("Welcome to the Home Page");
         this.add(label);
         this.setSize(400, 300);
